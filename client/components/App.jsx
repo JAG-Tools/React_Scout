40c1848f62b4bb91e374ead89cc8733cccdc79ab
Aum Dutta <saurabhaumdutta@gmail.com>
2017-11-21 18:44:27 -0500

    working on getting data from test to d3

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
-
+import myD3 from './myD3Tree';
+import reactComponents from './test';
+import App2 from './AppSample';
 export default class App extends React.Component {
     render() {
+        console.log('this is my array of objs', reactComponents.getData());
+        // localStorage.setItem('myStorage', JSON.stringify({aum: 'name'}));
+        // myD3.test(reactComponents.getData());
         return (
-        <div style={{textAlign: 'center'}}>
+        <div>
           <h1>Let's build this.....</h1>
+      
         </div>)
     }
 }
---
+++ client/components/test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+//connnect to app.jsx and import d3 function from apjsx
+//call 
+const reactComponents = {};
+
+reactComponents.getData = function() {
+let nodeList = [];  
+let mytree;
+
+class TreeNode {
+  constructor(name) {
+    this.label = name;
+    this._children = [];
+  }
+}
+
+React.Component.prototype.componentWillMount = function() {
+  
+  nodeList.push(new TreeNode(this.constructor.name));
+  // console.log(` Will Mount: ${this.constructor.name}`)
+  this.componentDidMount = function() {
+   console.log('hello')
+    for (let i = 0; i < nodeList.length; i += 1) {
+      if (nodeList.length === 1) {
+    
+        // mytree = nodeList.pop();
+        break;
+      }
+      if (nodeList[i].label === this.constructor.name) {
+        nodeList[i - 1]._children.push(nodeList[i])
+   
+        nodeList.splice(i, 1);
+      }
+    }
+    // console.log(` Did Mount: ${this.constructor.name}`)
+    if (this.constructor.name === 'App') console.log(mytree);
+    
+  }
+ }
+ return nodeList; 
+}
+
+
+
+const Component = React.Component;
+
+export default reactComponents;
+export { React, Component };
