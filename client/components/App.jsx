e8f6f5865e64cbe4ed1127f0be4de6196aa8bfef
Aum Dutta <saurabhaumdutta@gmail.com>
2017-11-28 20:42:26 -0500

    update static d3 tree with dummy object

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import unlace from 'unlace';
-import reactComponents from './test';
+// import unlace from 'unlace';
+import {reactComponents, myTreeDummy} from './test';
 import myD3 from './myD3Tree';
 import App2 from './AppSample';
 export default class App extends React.Component {
     render() {
-        let mytreesample = reactComponents.getData();
-        console.log('this is my array of objs', mytreesample);
+        // console.log(myTreeDummy)
+        // let mytreesample = reactComponents.getData();
+        // console.log('this is my array of objs', mytreesample);
         // localStorage.setItem('myStorage', JSON.stringify({aum: 'name'}));
         // let myjsonObj = unlace(mytreesample);
         // console.log(myjsonObj);
-        // console.log(myD3.test());
+        let myTree = myTreeDummy;
+        console.log('this is my tree in app.jsx', myTree)
+        
         return (
         <div>
-          <h1>Let's build this.....</h1>
-          <App2 />
+          
+          {/* <App2 /> */}
+          <div>{myD3.myTree(myTree)}</div>
         </div>)
     }
 }
--- client/components/test.js
+++ client/components/test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import unlace from 'unlace';
 
 //connnect to app.jsx and import d3 function from apjsx
 //call 
@@ -11,7 +12,7 @@ let mytree;
 class TreeNode {
   constructor(name) {
     this.label = name;
-    this._children = [];
+    this.children = [];
   }
 }
 
@@ -20,7 +21,7 @@ React.Component.prototype.componentWillMount = function() {
   nodeList.push(new TreeNode(this.constructor.name));
   // console.log(` Will Mount: ${this.constructor.name}`)
   this.componentDidMount = function() {
-   console.log('hello')
+  //  console.log('hello')
     for (let i = 0; i < nodeList.length; i += 1) {
       if (nodeList.length === 1) {
     
@@ -28,7 +29,7 @@ React.Component.prototype.componentWillMount = function() {
         break;
       }
       if (nodeList[i].label === this.constructor.name) {
-        nodeList[i - 1]._children.push(nodeList[i])
+        nodeList[i - 1].children.push(nodeList[i])
    
         nodeList.splice(i, 1);
       }
@@ -42,40 +43,13 @@ React.Component.prototype.componentWillMount = function() {
  return nodeList;
 }
 
-function changeLabels(obj) {
-  for (let property in obj) {
-   if (obj.hasOwnProperty(property)) {
-    if (typeof obj[property] == "object") {
-     //console.log( " it is an object ",  obj[property])
-     changeLabels(obj[property]);
-     if (property === "_children") {
-      obj["children"] = obj[property];
-      delete obj[property];
-     }
-    } else {
- 
-     if (property === "label") {
-      obj["name"] = obj[property];
-      delete obj[property];
-     }
- 
-     //console.log(property , "   " , obj[property]);
- 
-    }
- 
-   }
- 
-  }
- 
- }
+
+let myTreeDummy = [{"name":"App","children":[{"name":"HeaderX","children":[{"name":"ImageHeaderX","children":[],"parent":"HeaderX"},{"name":"AppTitleHeaderX","children":[],"parent":"HeaderX"}],"parent":"App"},{"name":"AppIntro","children":[],"parent":"App"},{"name":"FooterX","children":[{"name":"ImageFooterX","children":[],"parent":"FooterX"},{"name":"AppTitleFooterX","children":[],"parent":"FooterX"}],"parent":"App"}]}]
 
 
  const Component = React.Component;
  
- export default reactComponents;
- export { React, Component };
+//  export default Component;
+ export { React, Component, myTreeDummy};
 
   
-
-
-
