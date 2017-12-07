b8604220dcdb87ce730efb61edf8eb4c464819d7
santanaG <gs8777@gmail.com>
2017-12-07 00:31:44 -0500

    adjusts ignores and wires up CLI

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+// import { exec } from 'child_process';
+
+
+// export default () => {
+//   exec('node server.js', (err, stdout) => stdout);
+// };
+
+console.log('CLI functionality wired correctly!');
